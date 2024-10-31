e0d69da50c77440c82888557f1544b25cb0877b4
Russell Bricknell <brickhead92@gmail.com>
2024-10-31 22:15:20 +1000

    testing basic class

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-
-
+"use strict";
+let test, test2;
 function setup() {
+  let testGroup =new Group();
   createCanvas(400, 400);
+  world.gravity = 10;
+  test = new Throwable(200,200,80,40);
+  test.setup();
+  // test2 = new Sprite();
 }
 
 function draw() {
   background(220);
-  rect(200,200,80,40);
+  rect(50,50,300,100);
+  // test.draw();
 }
--- throw.js
+++ throw.js
@@ -1,6 +1,22 @@
 
-export class Test {
+class Throwable {
   constructor(x,y,w,h) {
-    rect(x,y,w,h);
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+  }
+  setup(){
+    this.sprite = new Sprite(this.w,this.h);
+    this.sprite.x = this.x
+    this.sprite.y = this.y
+    console.log(this)
+  }
+  move(){
+
+  }
+  update(){
+    // rect(this.x,this.y,this.w,this.h);
+    // this.sprite.direction
   }
 }
