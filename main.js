f4b6d37ad764f35116ff1b3976cbf4324aef547d
Russell Bricknell <russell.bricknell@universalfieldrobots.com.au>
2024-10-31 16:58:44 +1000

    initial upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  rect(200,200,80,40);
+}
---
+++ throw.js
@@ -0,0 +1,6 @@
+
+export class Test {
+  constructor(x,y,w,h) {
+    rect(x,y,w,h);
+  }
+}
