eb214ce6e7d2b5580ea4438dd610a3f31f479828
Russell Bricknell <brickhead92@gmail.com>
2024-11-02 21:28:04 +1000

    Updated moving player around, throwables from player position, and aim angle and power from holding space

--- main.js
+++ main.js
@@ -1,19 +1,75 @@
 "use strict";
-let test, test2;
+let test, test2, axis
+let aimed = false
+let rotation = 0
+let horizontal_direction = 0.5;
+let horizontal_position = 0
+let move_position = 200
+let throw_angle = 90;
+let throw_power = 0;
+let playerX = 200
+let playerY = 375
+
+
 function setup() {
-  let testGroup =new Group();
+  let testGroup = new Group();
   createCanvas(400, 400);
-  world.gravity = 10;
-  test = new Throwable(200,200,80,40);
-  test.setup();
-  // test2 = new Sprite();
+  world.gravity.y = 10;
 }
 
 function draw() {
   background(220);
-  rect(50,50,300,100);
-  // test.update();
+  rect(50, 50, 300, 150);
+  if (kb.presses(' ')) {
+    aimed = true
+    throw_angle = rotation - 90
+  }
+  if (aimed == false) {
+    if (horizontal_position <= -20) {
+      horizontal_direction = -horizontal_direction
+    }
+    if (horizontal_position >= 20) {
+      horizontal_direction = -horizontal_direction
+    }
 
-  test = new Sprite()
-
+  }
+  if (keyIsDown(32)) {
+    throw_power = kb.space
+  } else {
+    horizontal_position = horizontal_position + horizontal_direction
+    rotation += horizontal_direction
+  }
+  if (kb.pressing('up') && playerY >= canvas.height * 0.75) {
+    playerY -= 1
+  }
+  if (kb.pressing('down') && playerY <= canvas.height) {
+    playerY += 1
+  }
+  if (kb.pressing('left') && playerX >= 10) {
+    playerX -= 1
+  }
+  if (kb.pressing('right') && playerX <= canvas.width - 10) {
+    playerX += 1
+  }
+  push()
+  translate(playerX, playerY)
+  push()
+  rotate(rotation)
+  arc(0, -20, 40, 30, 75, 105, PIE)
+  pop()
+  pop()
+  if (kb.released(' ')) {
+    console.log("tp", throw_power)
+    while (true) {
+      if (throw_power > 60) {
+        throw_power /= 2.5
+      }
+      if (throw_power < 60) {
+        break
+      }
+    }
+    test = new Throwable(playerX, playerY, 80, 40, throw_angle, throw_power);
+    test.setup();
+    aimed = false
+  }
 }
--- throw.js
+++ throw.js
@@ -1,17 +1,22 @@
 
 class Throwable {
-  constructor(x,y,w,h) {
+  constructor(x,y,w,h, angle, power) {
     this.x = x
     this.y = y
     this.w = w
     this.h = h
+    this.angle = angle
+    this.power = power
   }
   setup(){
-    this.sprite = new Sprite(this.w,this.h);
+    this.sprite = new Sprite();
+    this.sprite.diameter = 10
     this.sprite.x = this.x
     this.sprite.y = this.y
-    this.sprite.show()
-    console.log(this)
+    this.sprite.direction = this.angle
+    this.sprite.rotation = this.angle
+    this.sprite.speed = 10
+    this.sprite.life = 110 - this.power
   }
   move(){
 
