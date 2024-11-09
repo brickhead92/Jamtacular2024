8df86e6e8bb692dbed5223d6c035314017286227
Russell Bricknell <brickhead92@gmail.com>
2024-11-10 08:41:57 +1000

    Update throw.js

--- throw.js
+++ throw.js
@@ -1,54 +1,23 @@
 
-
-// class Throwable {
-//   constructor(x,y,w,h, angle, power) {
-//     this.x = x
-//     this.y = y
-//     this.w = w
-//     this.h = h
-//     this.angle = angle
-//     this.power = power
-//   }
-//   setup(){
-//     this.sprite = new Sprite();
-//     this.sprite.overlaps(building.group)
-//     this.sprite.diameter = 10
-//     this.sprite.x = this.x
-//     this.sprite.y = this.y
-//     this.sprite.direction = this.angle
-//     this.sprite.rotation = this.angle
-//     this.sprite.speed = 10
-//     this.sprite.life = 110 - this.power
-//   }
-//   move(){
-
-//   }
-//   update(){
-//     // rect(this.x,this.y,this.w,this.h);
-//     this.sprite.direction
-//   }
-// }
-
-
 class Throwables {
   constructor() {
-    this.group
+    this.group;
   }
   setup(){
-    this.group = new Group()
+    this.group = new Group();
   }
   throw(x,y, angle, power, overlaps){
-    let throwable = new this.group.Sprite()
-    throwable.overlaps(this.group)
-    throwable.overlaps(overlaps)
-    throwable.diameter = 10
-    throwable.x = x
-    throwable.y = y
-    throwable.direction = angle
-    throwable.rotation = angle
-    throwable.type = "egg"
-    throwable.speed = 10
-    throwable.life = 110 - power
+    let throwable = new this.group.Sprite();
+    throwable.overlaps(this.group);
+    throwable.overlaps(overlaps);
+    throwable.diameter = 10;
+    throwable.x = x;
+    throwable.y = y;
+    throwable.direction = angle;
+    throwable.rotation = angle;
+    throwable.type = "egg";
+    throwable.speed = 10;
+    throwable.life = 110 - power;
   }
   hit() {
 
@@ -60,8 +29,8 @@ class Throwables {
     for (let i = 0; i < this.group.length; i++) {
       if (this.group[i].life == 1) {
         if (this.group[i].overlapping(building)) {
-          player.score += 100
-          console.log(player.score)
+          player.score += 100;
+          console.log(player.score);
         }
       }
     }
