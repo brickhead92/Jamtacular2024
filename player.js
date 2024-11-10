e7e6da72ccdef7d4a6cb1d1a227dc111f09bc85a
Russell Bricknell <brickhead92@gmail.com>
2024-11-10 12:14:05 +1000

    Update player.js

--- player.js
+++ player.js
@@ -10,7 +10,7 @@ class Player {
     this.throw_angle = 0;
     this.throw_power = 0;
     this.score = 0;
-    this.image = image
+    this.addImage(image);
   }
   update(throwable) {
     if (kb.presses(' ')) {
