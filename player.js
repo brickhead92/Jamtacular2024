8fa45dc4460fb883d799b310aaf85b787edc864b
Russell Bricknell <brickhead92@gmail.com>
2024-11-10 13:31:05 +1000

    Update player.js

--- player.js
+++ player.js
@@ -36,15 +36,19 @@ class Player {
     }
     if (kb.pressing('up') && this.y >= canvas.height * 0.75) {
       this.y -= 1;
+      this.sprite.y -= 1;
     }
     if (kb.pressing('down') && this.y <= canvas.height) {
       this.y += 1;
+      this.sprite.y += 1;
     }
     if (kb.pressing('left') && this.x >= 10) {
       this.x -= 1;
+      this.sprite.x -= 1;
     }
     if (kb.pressing('right') && this.x <= canvas.width - 10) {
       this.x += 1;
+      this.sprite.x += 1;
     }
     push();
     translate(this.x, this.y);
